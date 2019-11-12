b19f4ffbc500135a82a8251458991aad97b61317
SergeJey <sergejey@gmail.com>
2019-11-12 12:44:40 +0300

    Updates
    * Playnow update

--- modules/shoutcast/shoutcast.class.php
+++ modules/shoutcast/shoutcast.class.php
@@ -405,7 +405,7 @@ function api_call($path, $params=0) {
             $url.='&'.$k.'='.urlencode($v);
         }
 
-    $cached_filename=ROOT.'cached/shoutcast_'.md5($url).'.txt';
+    $cached_filename=ROOT.'cms/cached/shoutcast_'.md5($url).'.txt';
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
@@ -416,7 +416,7 @@ function api_call($path, $params=0) {
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
     curl_setopt($ch, CURLOPT_TIMEOUT, 15);
 
-    $tmpfname = ROOT . 'cached/cookie.txt';
+    $tmpfname = ROOT . 'cms/cached/cookie.txt';
     curl_setopt($ch, CURLOPT_COOKIEJAR, $tmpfname);
     curl_setopt($ch, CURLOPT_COOKIEFILE, $tmpfname);
 
